9d64d04ba700c9153e2736a2d6da9b1a484abfda
BOUACHRA <karima.bouachra@capgemini.com>
2026-07-21 17:08:50 +0100

    Initial commit

---
+++ src/app/features/vehicules/vehicules.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { Vehicules } from './vehicules';
+
+describe('Vehicules', () => {
+  let component: Vehicules;
+  let fixture: ComponentFixture<Vehicules>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [Vehicules],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(Vehicules);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/features/vehicules/vehicules.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vehicules',
+  imports: [],
+  templateUrl: './vehicules.html',
+  styleUrl: './vehicules.scss',
+})
+export class Vehicules {}
